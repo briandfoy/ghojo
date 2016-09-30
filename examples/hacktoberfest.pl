9d325c7096ccc9298ea035c8e0f80623a1fcc5be
brian d foy <brian.d.foy@gmail.com>
2016-09-30 17:50:28 -0400

    Put hacktoberfest to the examples/ dir

---
+++ examples/hacktoberfest.pl
@@ -0,0 +1,35 @@
+#!/Users/brian/bin/perls/perl5.24.0
+use v5.24;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+
+use lib qw(lib);
+
+use Ghojo;
+
+use Data::Dumper;
+
+my $hash = {
+	username => 'briandfoy',
+	password => 'tritonX100',
+	};
+
+my $ghojo = Ghojo->new( {} );
+$ghojo->logger->level( 'TRACE' );
+
+my $callback = sub ( $hashref ) {
+	unless( ref $hashref eq ref {} ) {
+		$ghojo->logger->error( "Not a hashref!" );
+		return;
+		}
+	my( $user, $repo ) = split m{/}, $hashref->{full_name};
+	my $owner = $hashref->{owner}{login};
+	return unless $self->username eq $owner;
+	[ $user, $repo ]
+	};
+
+my $query = {};
+
+my $repos = $ghojo->repos( $callback, $query );
+
+say Dumper( $repos );
