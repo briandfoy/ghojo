1390711f2fb7c88b5a67cf08cb970fe2731b0351
brian d foy <brian.d.foy@gmail.com>
2016-09-30 21:58:12 -0400

    Add a Repo would that will wrap some calls
    Many calls take OWNER and REPO params as their 
    first arguments. This will fill in those for you.
    
    You can't use the logged in name as the OWNER because
    some of these methods let you look at public info for
    any user. This way, you can create a repo object that
    has the owner and repo you want.

---
+++ lib/Ghojo/Repo.pm
@@ -0,0 +1,41 @@
+use v5.24;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+
+package Ghojo::Repo 0.001001 {
+	use Carp qw(carp);
+
+	sub new_from_response ( $class, $ghojo, $response ) {
+		my( $owner, $repo ) = split m{/}, $response->{full_name};
+		bless {
+			ghojo => $ghojo,
+			owner => $owner,
+			repo  => $repo,
+			data  => $response
+			}, $class;
+		}
+
+	sub ghojo ( $self ) { $self->{ghojo} }
+	sub owner ( $self ) { $self->{owner} }
+	sub repo  ( $self ) { $self->{repo}  }
+	sub data  ( $self ) { $self->{data}  }
+
+	sub AUTOLOAD ( $self ) {
+		our $AUTOLOAD;
+		my $method = $AUTOLOAD =~ s/.*:://r;
+		unless( $self->ghojo->can( $method ) ) {
+			carp "Cannot locate method $method";
+			}
+
+		$self->ghojo->logger->trace(
+			sprintf "Calling $method with <%s %s> <@_>",
+				$self->owner,
+				$self->repo
+			 );
+		$self->ghojo->$method( $self->owner, $self->repo, @_ );
+		}
+
+	sub DESTROY { }
+	}
+
+1;
---
+++ t/get_repo.t
@@ -0,0 +1,34 @@
+use Test::More 0.95;
+
+my $class      = 'Ghojo';
+my $repo_class = 'Ghojo::Repo';
+my $method     = 'get_repo_object';
+my $user       = 'BurnItToTheGround';
+my $repo       = 'test_repo';
+my $bad_repo   = 'is_not_there';
+
+subtest setup => sub {
+	use_ok $class;
+	can_ok $class, $method;
+	};
+
+subtest good_repo => sub {
+	my $ghojo = $class->new;
+	isa_ok $ghojo, $class;
+
+	my $repo  = $ghojo->$method( $user, $repo );
+	isa_ok( $repo, $repo_class );
+	can_ok( $repo, 'data' );
+	ok( exists $repo->data->{full_name}, "full_name key exists" );
+	diag( "Repo name is " . $repo->data->{full_name} );
+	};
+
+subtest bad_repo => sub {
+	my $ghojo = $class->new;
+	isa_ok $ghojo, $class;
+	$ghojo->logger->level( 'OFF' );
+	my $rc = $ghojo->$method( $user, $bad_repo );
+	ok( ! defined $rc, "bad repo returns undef" );
+	};
+
+done_testing();
