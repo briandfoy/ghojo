1d2bcd981e082334fa1f48b300d98e68cd7549b6
brian d foy <brian.d.foy@gmail.com>
2016-10-03 19:14:40 -0700

    Example of user things

---
+++ examples/users.pl
@@ -0,0 +1,40 @@
+#!/Users/brian/bin/perls/perl5.24.0
+use v5.24;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+
+use lib qw(lib);
+use FindBin;
+use lib $FindBin::Bin;
+
+BEGIN { require 'common_things.pl' }
+
+use Ghojo;
+
+my $hash = {
+	username => username(),
+	password => password(),
+	};
+
+say "log_level is " . log_level();
+
+$ENV{GHOJO_LOG_LEVEL} = log_level();
+my $ghojo = Ghojo->new( $hash );
+
+say Dumper( $ghojo->get_logged_in_user );
+
+say '-' x 50;
+
+say Dumper( $ghojo->get_user( 'briandfoy' ) );
+
+say '-' x 50;
+
+$ghojo->set_paged_get_results_limit( 23 );
+
+my $callback = sub ( $item ) {
+	my %hash = $item->%{qw(name login email)};
+	say Dumper( \%hash );
+	$hash;
+	};
+
+$ghojo->get_all_users( $callback )
