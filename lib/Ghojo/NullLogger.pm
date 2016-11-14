db51e0fe0d08a6bbb574f556ac20b540633b9cde
brian d foy <brian.d.foy@gmail.com>
2016-11-13 18:58:30 -0800

    Note explicitly that this is Artistic License 2.
    That's not under the same terms as Perl! There's
    no icky GPL here.

--- lib/Ghojo/NullLogger.pm
+++ lib/Ghojo/NullLogger.pm
@@ -35,7 +35,8 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 Copyright © 2016, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+it under the Artistic License 2. A LICENSE file should have accompanied
+this distribution.
 
 =cut
 
