4a2d3e86f1b00f385e805156ca71746300b03e0f
brian d foy <brian.d.foy@gmail.com>
2016-11-13 19:34:53 -0800

    Add stubs for other parts of the API

---
+++ lib/Ghojo/Endpoint/Activity.pm
@@ -0,0 +1,51 @@
+use v5.24;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+
+package Ghojo::Endpoint::Activity;
+
+our $VERSION = '1.001001';
+
+use Mojo::Collection;
+use Mojo::URL;
+
+=encoding utf8
+
+=head1 NAME
+
+Ghojo::Endpoint::Activity - The endpoints that deal with activity
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+=head2  General object thingys
+
+=over 4
+
+=item *
+
+=back
+
+=head1 SOURCE AVAILABILITY
+
+This module is in Github:
+
+	git://github.com/briandfoy/ghojo.git
+
+=head1 AUTHOR
+
+brian d foy, C<< <bdfoy@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2016, brian d foy <bdfoy@cpan.org>. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the Artistic License 2. A LICENSE file should have accompanied
+this distribution.
+
+=cut
+
+__PACKAGE__
