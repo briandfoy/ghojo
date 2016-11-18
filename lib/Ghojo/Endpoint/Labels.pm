92a5121de83e895497a1ac79afea1fc830d8c8b8
brian d foy <brian.d.foy@gmail.com>
2016-11-18 00:12:41 -0600

    Update Labels to the new fetching stuff (single_resource) etc.

--- lib/Ghojo/Endpoint/Labels.pm
+++ lib/Ghojo/Endpoint/Labels.pm
@@ -28,155 +28,115 @@ Ghojo::Endpoint::Labels - The endpoints that deal with labels
 
 =head1 DESCRIPTION
 
-=head2 Labels
+This section implements the endpoints that deal with labels.
 
-    # http://developer.github.com/v3/issues/labels/
-    labels => { url => "/repos/%s/%s/labels" },
-    label  => { url => "/repos/%s/%s/labels/%s" },
-    create_label => { url => "/repos/%s/%s/labels", method => 'POST', args => 1 },
-    update_label => { url => "/repos/%s/%s/labels/%s", method => 'PATCH', args => 1 },
-    delete_label => { url => "/repos/%s/%s/labels/%s", method => 'DELETE', check_status => 204 },
-    issue_labels => { url => "/repos/%s/%s/issues/%s/labels" },
-    create_issue_label  => { url => "/repos/%s/%s/issues/%s/labels", method => 'POST', args => 1 },
-    delete_issue_label  => { url => "/repos/%s/%s/issues/%s/labels/%s", method => 'DELETE', check_status => 204 },
-    replace_issue_label => { url => "/repos/%s/%s/issues/%s/labels", method => 'PUT', args => 1 },
-    delete_issue_labels => { url => "/repos/%s/%s/issues/%s/labels", method => 'DELETE', check_status => 204 },
-    milestone_labels => { url => "/repos/%s/%s/milestones/%s/labels" },
+=head2 Labels
 
 =over 4
 
 =cut
 
-=item * labels( USER, REPO )
+=item * labels( OWNER, REPO [, CALLBACK] )
 
-Get the information for all the labels of a repo.
+Get the information for all the labels of a repo. If the result is a
+success, the value is a L<Mojo::Collection> object representing the
+list of labels.
+
+	my $results = $ghojo->labels( $owner, $repo );
+	if( $results->is_success ) {
+		$results->values->each( sub { say $_->name } );
+		}
+
+	# or with a callback
+	my $results = $ghojo->labels( $owner, $repo, $callback );
 
 Returns a L<Mojo::Collection> object with the hashrefs representing
-labels:
+labels. Each item is a hash blessed into C<Ghojo::Data::Label>.
 
-	{
-	'color' => 'd4c5f9',
-	'url' => 'https://api.github.com/repos/briandfoy/test-file/labels/Perl%20Workaround',
-	'name' => 'Perl Workaround'
-	}
-
-Implements C</repos/:owner/:repo/labels>.
+This is a public API endpoint.
 
 =cut
 
-sub Ghojo::PublicUser::labels ( $self, $owner, $repo ) {
-	$self->get_single_resource(
-		endpoint        => '/repos/%s/%s/labels',
+sub Ghojo::PublicUser::labels ( $self, $owner, $repo, $callback = sub { $_[0] } ) {
+	$self->get_paged_resources(
+		endpoint        => '/repos/:owner/:repo/labels',
 		endpoint_params => { owner => $owner, repo => $repo },
+		callback        => $callback,
+		bless_into      => 'Ghojo::Data::Label',
 		);
 	}
 
-=item * get_label( USER, REPO, LABEL )
+=item * get_label( USER, REPO, LABEL_NAME )
 
-Get the information for a particular label. It returns a hashref:
+Get the information for a particular label.
 
-	{
-	'color' => '1d76db',
-	'url' => 'https://api.github.com/repos/briandfoy/test-file/labels/Win32',
-	'name' => 'Win32'
-	}
-
-Implements C</repos/:owner/:repo/labels/:name>.
+This is a public API endpoint.
 
 =cut
 
-sub Ghojo::PublicUser::get_label ( $self, $user, $repo, $name ) {
-	state $expected_status = 200;
-	my $params = [ $user, $repo, $name ];
-	my $url = $self->query_url( "/repos/%s/%s/labels/%s", $params );
-	$self->logger->trace( "Query URL is $url" );
-
-	my $tx = $self->ua->get( $url );
-	unless( $tx->code eq $expected_status ) {
-		$self->logger->error( sprintf "get_label returned status %s but expected %s", $tx->code, $expected_status );
-		$self->logger->debug( "get_label response for [ $user, $repo, $name ] was\n", $tx->res->body );
-		return Ghojo::Result->error;
-		}
-
-	$tx->res->json;
+sub Ghojo::PublicUser::get_label ( $self, $owner, $repo, $name ) {
+	$self->get_single_resource(
+		endpoint        => '/repos/:owner/:repo/labels/:name',
+		endpoint_params => { owner => $owner, repo => $repo, name => $name },
+		bless_into      => 'Ghojo::Data::Label',
+		);
 	}
 
-=item * create_label
+=item * create_label( OWNER, REPO, LABEL_NAME [, COLOR] )
 
-Create a new label.
+Create a new label. The LABEL_NAME is a string and the COLOR is
+a six digit hexadecimal RGB color specification (without the #).
+The default color is C<FF0000>.
 
-Query parameters
-
-	name	string	(Required) The name of the label.
-	color	string	(Required) A 6 character RGB color
-	                Default is FF0000
-
-Implements C<POST /repos/:owner/:repo/labels>.
+This is an authenticated endpoint.
 
 =cut
 
 sub Ghojo::AuthenticatedUser::create_label ( $self, $owner, $repo, $name, $color = 'FF0000' ) {
-	state $expected_status = 201;
-	my $params             = [ $owner, $repo ];
-
 	$color =~ s/\A#//;
 
-	my $query = {
-		name  => $name,
-		color => $color,
-		};
-	my $url = $self->query_url( "/repos/%s/%s/labels", $params, $query );
-
-	$self->logger->trace( "Query URL is $url" );
-	my $tx = $self->ua->post( $url => json => $query );
-	my $code = $tx->res->code;
-
-	unless( $code == $expected_status ) {
-		my $body = $tx->res->body;
-		$self->logger->error( "create_label did not return a $expected_status: $body" );
-		return 0;
-		}
-
-	return 1;
+	$self->post_single_resource(
+		endpoint        => '/repos/:owner/:repo/labels',
+		endpoint_params => { owner => $owner, repo => $repo },
+		json    => {
+			name  => $name,
+			color => $color,
+			},
+		);
 	}
 
-=item * update_label( OWNER, REPO, NAME, NEW_NAME, NEW_COLOR )
+=item * update_label( OWNER, REPO, NAME, HASH )
 
-PATCH /repos/:owner/:repo/labels/:name
+This one is a bit tricky. You need to specify the current name, but
+also the new name or new color (or both).
 
-JSON parameters
+	# update both name and color
+	$ghojo->update_label( $owner, $repo, $name, { name => $new_name, color => $color } );
 
-	name	string	The name of the label.
-	color	string	A 6 character hex code, without the leading #, identifying the color.
+	# update color only
+	$ghojo->update_label( $owner, $repo, $name, { color => $color } );
 
-Response Status: 200 OK
+	# update name only
+	$ghojo->update_label( $owner, $repo, $name, { name => $new_name } );
+
+This is an authenticated endpoint.
 
 =cut
 
-sub Ghojo::AuthenticatedUser::update_label ( $self, $owner, $repo, $name, $new_name, $color ) {
-	state $expected_status = 200;
-	my $params             = [ $owner, $repo, $name ];
+sub Ghojo::AuthenticatedUser::update_label ( $self, $owner, $repo, $name, $args ) {
+	$args->{color} =~ s/\A#// if exists $args->{color};
 
-	$color =~ s/\A#//;
-
-	my $query = {};
-
-	$query->{name}  = $new_name if defined $new_name;
-	$query->{color} = $color    if defined $color;
-
-	my $url = $self->query_url( "/repos/%s/%s/labels/%s", $params );
-
-	$self->logger->trace( "update_label: URL is $url" );
-	my $tx = $self->ua->patch( $url => json => $query );
-	my $code = $tx->res->code;
-
-	unless( $code == $expected_status ) {
-		my $body = $tx->res->body;
-		$self->logger->error( "create_label did not return a $expected_status: $body" );
-		return 0;
-		}
-
-	return 1;
+	$self->patch_single_resource(
+		endpoint        => '/repos/:owner/:repo/labels/:name',
+		endpoint_params => { owner => $owner, repo => $repo, name => $name },
+		json            => $args,
+		query_profile   => {
+			params => {
+				name  => qr/\S/,
+				color => qr/\A[0-9a-f]{6}\z/i,
+				},
+			},
+		);
 	}
 
 =item * delete_label( OWNER, REPO, LABEL_NAME )
@@ -208,33 +168,21 @@ sub Ghojo::AuthenticatedUser::delete_label ( $self, $owner, $repo, $name ) {
 
 =item * get_labels_for_issue( OWNER, REPO, ISSUE_NUMBER, CALLBACK )
 
-GET /repos/:owner/:repo/issues/:number/labels
-
-Response
-Status: 200 OK
-Link: <https://api.github.com/resource?page=2>; rel="next",
-      <https://api.github.com/resource?page=5>; rel="last"
-
-[
-  {
-    "url": "https://api.github.com/repos/octocat/Hello-World/labels/bug",
-    "name": "bug",
-    "color": "f29513"
-  }
-]
+This is a public API endpoint.
 
 =cut
 
 sub Ghojo::PublicUser::get_labels_for_issue ( $self, $owner, $repo, $number, $callback = sub { $_[0] } ) {
 	$self->get_paged_resources(
-		endpoint        => '/repos/:owner/:repo/issues/%d/labels',
+		endpoint        => '/repos/:owner/:repo/issues/:number/labels',
 		endpoint_params => { owner => $owner, repo => $repo, number => $number },
 		callback        => $callback,
+		bless_into      => 'Ghojo::Data::Label',
 		);
 	}
 
 
-=item * get_labels_for_all_issus_in_milestone
+=item * get_labels_for_all_issus_in_milestone( OWNER, REPO, MILESTONE_ID [, CALLBACK] )
 
 GET /repos/:owner/:repo/milestones/:number/labels
 Response
@@ -249,15 +197,17 @@ Link: <https://api.github.com/resource?page=2>; rel="next",
   }
 ]
 
+This is a public API endpoint.
+
 =cut
 
-sub Ghojo::PublicUser::get_labels_for_all_issus_in_milestone ( $self, $owner, $repo, $name ) {
-	$self->not_implemented;
-
-	state $expected_status = 200;
-	my $params             = [ $owner, $repo, $name ];
-
-
+sub Ghojo::PublicUser::get_labels_for_all_issus_in_milestone ( $self, $owner, $repo, $milestone, $callback = sub { $_[0] } ) {
+	$self->get_paged_resources(
+		endpoint        => '/repos/:owner/:repo/milestones/:number/labels',
+		endpoint_params => { owner => $owner, repo => $repo, milestone => $milestone },
+		callback        => $callback,
+		bless_into      => 'Ghojo::Data::Label',
+		);
 	}
 
 
@@ -287,90 +237,71 @@ Status: 200 OK
 	  }
 	]
 
+This is an authenticated endpoint.
+
 =cut
 
 sub Ghojo::AuthenticatedUser::add_labels_to_issue ( $self, $owner, $repo, $number, @names ) {
-	state $expected_status = 200;
-	my $params             = [ $owner, $repo, $number ];
-
-	my $url = $self->query_url( "/repos/%s/%s/issues/%s/labels", $params );
-
-	$self->logger->trace( "add_labels_to_issue: URL is $url" );
-	my $tx = $self->ua->post( $url => json => \@names );
-	my $code = $tx->res->code;
-
-	unless( $code == $expected_status ) {
-		my $body = $tx->res->body;
-		$self->logger->error( "add_labels_to_issue did not return $expected_status: $body" );
-		return 0;
-		}
-
-	return 1;
+	$self->post_single_resource(
+		endpoint        => '/repos/:owner/:repo/issues/:number/labels',
+		endpoint_params => { owner => $owner, repo => $repo, number => $number },
+		json            => \@names,
+		expected_http_status => 200,
+		);
 	}
 
 
-=item * remove_label_from_issue
+=item * remove_label_from_issue( OWNER, REPO, ISSUE_ID, LABEL_NAME )
 
 DELETE /repos/:owner/:repo/issues/:number/labels/:name
 Response
 Status: 204 No Content
 
+This is an authenticated endpoint.
+
 =cut
 
-sub Ghojo::AuthenticatedUser::remove_label_from_issue ( $self, $owner, $repo, $name ) {
-	$self->not_implemented;
-	state $expected_status = 204;
-	my $params             = [ $owner, $repo, $name ];
-
-
+sub Ghojo::AuthenticatedUser::remove_label_from_issue ( $self, $owner, $repo, $number, $name ) {
+	$self->delete_single_resource(
+		endpoint        => '/repos/:owner/:repo/issues/:number/labels/:name',
+		endpoint_params => { owner => $owner, repo => $repo, number => $number, name => $name },
+		);
 	}
 
 
-=item * remove_all_labels_from_issue
+=item * remove_all_labels_from_issue( OWNER, REPO, ISSUE_ID )
 
 DELETE /repos/:owner/:repo/issues/:number/labels
 Response
 Status: 204 No Content
 
+This is an authenticated endpoint.
+
 =cut
 
-sub Ghojo::AuthenticatedUser::remove_all_labels_from_issue ( $self, $owner, $repo, $name ) {
-	$self->not_implemented;
-	state $expected_status = 204;
-	my $params             = [ $owner, $repo, $name ];
-
-
+sub Ghojo::AuthenticatedUser::remove_all_labels_from_issue ( $self, $owner, $repo, $issue_id ) {
+	$self->delete_single_resource(
+		endpoint        => '/repos/:owner/:repo/issues/:number/labels',
+		endpoint_params => { owner => $owner, repo => $repo, number => $issue_id },
+		);
 	}
 
 
-=item * replace_all_labels_for_issue
+=item * replace_all_labels_for_issue( OWNER, REPO, ISSUE_NUMBER [, LABEL_NAME] );
 
-PUT /repos/:owner/:repo/issues/:number/labels
-Input
-[
-  "Label1",
-  "Label2"
-]
-Sending an empty array ([]) will remove all Labels from the Issue.
+Removes the named labels from the issue. If you don't specify any
+label names, it removes all of the labels.
 
-Response
-Status: 200 OK
-[
-  {
-    "url": "https://api.github.com/repos/octocat/Hello-World/labels/bug",
-    "name": "bug",
-    "color": "f29513"
-  }
-]
+This is an authenticated endpoint.
 
 =cut
 
-sub Ghojo::AuthenticatedUser::replace_all_labels_for_issue ( $self, $owner, $repo, $name ) {
-	$self->not_implemented;
-	state $expected_status = 200;
-	my $params             = [ $owner, $repo, $name ];
-
-
+sub Ghojo::AuthenticatedUser::replace_all_labels_for_issue ( $self, $owner, $repo, $number, @names ) {
+	$self->put_single_resource(
+		endpoint        => '/repos/:owner/:repo/issues/:number/labels',
+		endpoint_params => { owner => $owner, repo => $repo, number => $number },
+		json            => \@names,
+		);
 	}
 
 =back
