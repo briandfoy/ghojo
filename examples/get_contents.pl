dda1dee363948ef95a5b79f31b94bcb68d002901
brian d foy <brian.d.foy@gmail.com>
2017-04-10 02:24:00 -0400

    Expand the Contents endpoints to get any resource

---
+++ examples/get_contents.pl
@@ -0,0 +1,37 @@
+#!/Users/brian/bin/perls/perl5.24.0
+use v5.24;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+
+use lib qw(lib);
+
+use Ghojo;
+use Ghojo::Constants;
+use subs qw(FILE_NOT_FOUND);
+
+use Mojo::Util qw(dumper);
+
+my $ghojo = Ghojo->new;
+
+# the third parameter is a boolean for HTMLized contents
+my $result = $ghojo->get_contents(
+	'briandfoy',
+	'ghojo',
+	$ARGV[0] // 'Makefile.PL',
+	{
+	  'ref'     => $ARGV[1] // 'master',
+	}
+	);
+
+if( $result->is_success ) {
+	my $file = $result->single_value->content;
+	print $file;
+	}
+else {
+	if( $result->error_code eq RESOURCE_NOT_FOUND ) {
+		say "Object not found";
+		}
+	else {
+		say "Some unspecified error";
+		}
+	}
