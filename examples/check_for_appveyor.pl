ae88f671b84f13732911c6f3067da74939c4c9c9
brian d foy <brian.d.foy@gmail.com>
2017-04-10 06:31:23 -0400

    Check for appveyor or travis files

--- examples/check_for_appveyor.pl
+++ examples/check_for_appveyor.pl
@@ -3,12 +3,17 @@ use v5.24;
 use feature qw(signatures);
 no warnings qw(experimental::signatures);
 
-use lib qw(lib);
+use lib qw(lib examples);
 
 use Ghojo;
 use Mojo::Util qw(dumper);
 
-my $ghojo = Ghojo->new;
+BEGIN { require 'common_things.pl' }
+
+my $ghojo = go_go_ghojo();
+
+my %needs_appveyor;
+my %needs_travis;
 
 my $callback = sub( $item, $tx ) {
 	my $repo = $item->{name};
@@ -20,6 +25,28 @@ my $callback = sub( $item, $tx ) {
 		}
 
 	say "$repo has Makefile.PL";
+
+	if( $item->{'fork'} ) {
+		say "\t$repo is a fork";
+		return 0;
+		}
+
+	my $appveyor_result = $ghojo->get_contents( $owner, $repo, '.appveyor.yml' );
+	unless( $appveyor_result->is_success ) {
+		say "\tNo appveyor in $repo";
+		$needs_appveyor{$repo} = 1;
+		}
+
+	my $travis_result = $ghojo->get_contents( $owner, $repo, '.travis.yml' );
+	unless( $travis_result->is_success ) {
+		say "\tNo travis in $repo";
+		$needs_travis{$repo} = 1;
+		}
+
+	if( $appveyor_result->is_error or  $travis_result->is_error ) {
+		return 0;
+		}
+
 	return 1;
 	};
 
@@ -28,3 +55,5 @@ unless( $result->is_success ) {
 	say "ERROR: " . $result->message;
 	say "Error from " . $result->subroutine;
 	}
+
+say dumper( \%needs_appveyor, \%needs_travis );
