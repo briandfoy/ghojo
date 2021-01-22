77c72eb3f95c72969db99b790f8890fdb4d668da
brian d foy <brian.d.foy@gmail.com>
2021-01-21 20:31:52 -0500

    Use the new dumper routines

--- examples/delete_repo.pl
+++ examples/delete_repo.pl
@@ -11,17 +11,17 @@ use Mojo::Util qw(dumper);
 
 my( $owner, $repo ) = split m|/|, $ARGV[0];
 
-my $ghojo = Ghojo->new({
-	token => $ENV{GITHUB_TOKEN},
-	});
-
+my $ghojo = Ghojo->new({ token => $ENV{GITHUB_TOKEN} });
+if( $ghojo->is_error ) {
+	say $ghojo->short_summary;
+	exit;
+	}
 
 my $result = $ghojo->delete_repo( $owner, $repo );
-say dumper( $result );
+
 if( $result->is_success ) {
 	say "Repo deleted!";
 	}
 else {
-	say "Deleting repo failed. Message: " . $result->message;
-	exit;
+	say $result->long_summary;
 	}
