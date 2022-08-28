65382653bb76b757cf493c27770bd8e3f758681c
brian d foy <briandfoy@pobox.com>
2022-08-27 20:48:30 -0400

    Don't have plain hash in state (for v5.26)

--- lib/Ghojo/Scopes.pm
+++ lib/Ghojo/Scopes.pm
@@ -126,12 +126,12 @@ my sub _extract ( $tx, $header ) {
 =cut
 
 sub extract_scopes_from ( $class, $tx ) {
-	state %h = (
+	state $h = {
 		has      => 'x-oauth-scopes',
 		requires => 'x-accepted-oauth-scopes',
-		);
+		};
 
-	my %hash = map { $_, $class->new( _extract($tx, $h{$_}) ) } keys %h;
+	my %hash = map { $_, $class->new( _extract($tx, $h->{$_}) ) } keys %h;
 	return \%hash;
 	}
 
