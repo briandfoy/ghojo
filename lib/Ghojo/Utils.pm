0c71c5412c9249df30aa0d33825e47f62896a90f
brian d foy <briandfoy@pobox.com>
2024-01-03 14:47:05 -0500

    Add carp warnings a few places

--- lib/Ghojo/Utils.pm
+++ lib/Ghojo/Utils.pm
@@ -3,6 +3,7 @@ use v5.26;
 package Ghojo::Utils;
 use experimental qw(signatures);
 
+use Carp qw(carp);
 use Exporter   qw(import);
 use Mojo::Util qw(b64_encode dumper trim);
 use Mojo::JSON qw(decode_json);
@@ -21,12 +22,14 @@ our %EXPORT_TAGS = (
 	);
 
 sub dump_request ( $tx ) {
+	carp "Did not get a transaction!" unless ref $tx;
 	my $string = $tx->req->to_string;
 	$string =~ s/Authorization:\s+token\s+...\K.*/***/;
 	$string =~ s/Authorization:\s+basic\s+\K.*/***/r;
 	}
 
 sub dump_response ( $tx ) {
+	carp "Did not get a transaction!" unless ref $tx;
 	my $start_line = join ' ', $tx->res->code, 'HTTP/1.1';
 	$start_line . "\n" . $tx->res->headers->to_string
 	}
